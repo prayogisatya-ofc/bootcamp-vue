b5973fb9dcde9cc78a5213ce0cf72e7ebdf8d734
besutodio <besutodio@gmail.com>
2022-12-17 12:17:17 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data(){
+    return {
+      isGanteng: false
+    }
+  },
+  methods: {
+    ubah(){
+      this.isGanteng = !this.isGanteng
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <button @click="ubah()">Switch</button>
+    <h1 v-if="isGanteng">Ganteng dong!!</h1>
+    <h1 v-else>Kamu jelek!!</h1>
+  </main>
+</template>
